cfea1cdba5f15ee078ea51c22e2d61bd3c60c08b
Pat Wilson <zerostride@gmail.com>
2020-12-10 14:06:16 -0800

    Merge in event data

--- lib/logsformyfamily/logger.rb
+++ lib/logsformyfamily/logger.rb
@@ -6,6 +6,7 @@ module LogsForMyFamily
 
     def initialize
       @backends = []
+      @host_config = {}
     end
 
     %i[
@@ -24,11 +25,24 @@ module LogsForMyFamily
       end
     end
 
-    def internal_log(level, level_name, event_type, event_data)
+    def internal_log(_level, level_name, event_type, event_data)
+      timestamp = Time.now.to_f # Do this first before filtering or any other things
+
       # TODO: filter based on level
       # TODO: sampling based filtering
+
+      event_data = { message: event_data } unless event_data.is_a?(Hash)
+
+      merged_data = {
+        pid: Process.pid,
+        timestamp: timestamp,
+        thread_id: Thread.current.object_id
+      }
+                    .merge(@host_config)
+                    .merge(event_data)
+
       @backends.each do |backend|
-        backend.call(level_name, event_type, event_data)
+        backend.call(level_name, event_type, merged_data)
       end
     end
   end
--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe LogsForMyFamily::Logger do
     expect(last_log).not_to be nil
     expect(last_log.level).to eql :info
     expect(last_log.type).to eql 'foo'
-    expect(last_log.data).to eql 'bar'
+    expect(last_log.data).to include(message: 'bar')
   end
 
   context 'with multiple logging backends' do
@@ -32,12 +32,12 @@ RSpec.describe LogsForMyFamily::Logger do
       expect(last_log).not_to be nil
       expect(last_log.level).to eql :info
       expect(last_log.type).to eql 'foo'
-      expect(last_log.data).to eql 'bar'
+      expect(last_log.data).to include(message: 'bar')
 
       expect(last_log2).not_to be nil
       expect(last_log2.level).to eql :info
       expect(last_log2.type).to eql 'foo'
-      expect(last_log2.data).to eql 'bar'
+      expect(last_log2.data).to include(message: 'bar')
     end
   end
 end
